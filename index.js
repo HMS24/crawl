8a046c605ca47194adfbb63872ed3fc513f10c38
huangminshi <z7707092003@gmail.com>
2021-06-05 17:34:38 +0800

    Refactor index.js

--- index.js
+++ index.js
@@ -5,94 +5,56 @@ const {
 	deleteFiles,
 	writeFile,
 } = require('./lib/fs');
+const {
+	scroll,
 
-const BROWSER_OPTIONS = {
-	headless: false,
-};
-const NAVIGATION_OPTIONS = {
-	waitUntil: 'networkidle2',
-};
-const VIEWPORT = {
-	width: 1440,
-	height: 768,
-};
-const LIMIT = 30;
+	BROWSER_OPTION,
+	NAVIGATION_OPTION,
+	VIEWPORT_OPTION,
+} = require('./lib/page');
+const {
+	crawl,
+	getTotalRows,
+	generateNextPageURL,
+
+	BASE_URL,
+	ROWS_PER_PAGE,
+} = require('./lib/house-sale-websites/591');
+
 const OUT_DIR = 'output/test';
 const OUT_FILE = `./${OUT_DIR}/crawl.json`;
-const BASE_URL = 'https://sale.591.com.tw/?shType=list&regionid=3&section=50&kind=9&price=1000$_1200$&shape=2&pattern=3&area=40_50&houseage=$_10$&order=price_desc';
 
 (async () => {
 	try {
-		const browser = await puppeteer.launch(BROWSER_OPTIONS);
+		const browser = await puppeteer.launch(BROWSER_OPTION);
 		const page = await browser.newPage();
 
-		await page.setViewport(VIEWPORT);
-		await page.goto(BASE_URL, NAVIGATION_OPTIONS);
+		await page.setViewport(VIEWPORT_OPTION);
+		await page.goto(BASE_URL, NAVIGATION_OPTION);
 		await page.click('#app');
 
+		const totalRows = await getTotalRows(page);
+		const paginationNum = parseInt(Math.ceil(totalRows / ROWS_PER_PAGE));
 		const results = [];
-		const totalRows = await page.evaluate(() => document.querySelector('.houseList-head-title em').textContent);
-		const paginationNum = parseInt(Math.ceil(totalRows / LIMIT));
 
 		for (let currentPage = 1; currentPage <= paginationNum; currentPage++) {
 			if (currentPage > 1) {
-				const offset = (currentPage - 1) * LIMIT;
+				const offset = (currentPage - 1) * ROWS_PER_PAGE;
+				const nextPage = generateNextPageURL({
+					baseURL: BASE_URL,
+					firstRow: offset,
+					totalRows,
+				});
 
-				await page.goto(`${BASE_URL}&firstRow=${offset}&totalRows=${totalRows}`, NAVIGATION_OPTIONS);
+				await page.goto(nextPage, NAVIGATION_OPTION);
 			}
 
-			await page.evaluate(async () => {
-				const SCROLL_NUM = 3;
-				const scrollHeight = document.body.scrollHeight;
-				const scrollHeightOffset = Math.ceil(scrollHeight / SCROLL_NUM);
+			await scroll(page);
 
-				for (let i = 0; i < SCROLL_NUM; i++) {
-					const top = (i + 1) * scrollHeightOffset;
-
-					window.scrollTo({
-						top,
-						behavior: 'smooth',
-					});
-
-					await new Promise(resolve => setTimeout(resolve, 1000));
-				}
-			});
-
-			const nodes = await page.evaluate(() => {
-				const nodes = [...document.querySelectorAll('.houseList-item')];
-
-				return nodes.map(node => {
-					const price = node.querySelector('.houseList-item-price em').textContent;
-					const section = node.querySelector('.houseList-item-section').textContent;
-					const address = node.querySelector('.houseList-item-address').textContent;
-
-					const { src: thumbURL } = node.querySelector('.houseList-item-thumb');
-					const { href, title } = node.querySelector('.houseList-item-title a');
-
-					const communityElement = node.querySelector('.houseList-item-community') ;
-					const community = communityElement === null ? null : communityElement.firstElementChild.textContent;
-
-					const [
-						shape, layout, area,
-						mainArea, houseAge, floor,
-					] = [...node.querySelector('.houseList-item-attr-row').children].map(node => node.textContent);
-
-					const tags = [...node.querySelector('.houseList-item-tag-row').children].map(element => element.textContent);
-
-					return {
-						title, price, community,
-						shape, layout, area,
-						mainArea, houseAge, floor,
-						section, address, tags,
-						href, thumbURL,
-					};
-				});
-			});
+			const nodes = await crawl(page);
 
 			results.push(...nodes);
 		}
-
-		results.forEach(e => e.price = dollarStringToInt(e.price));
 
 		createDirectoryIfNotExisted(OUT_DIR);
 
@@ -104,7 +66,3 @@ const BASE_URL = 'https://sale.591.com.tw/?shType=list&regionid=3&section=50&kin
 		console.log(error);
 	}
 })();
-
-function dollarStringToInt(value) {
-	return parseInt(value.split(',').join(''));
-}
